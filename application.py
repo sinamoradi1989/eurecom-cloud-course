f0724560eecea7daffdb1418fcd71221116a1f0b
sinamoradi1989 <94011603+sinamoradi1989@users.noreply.github.com>
2021-11-10 20:41:35 +0330

    Update application.py

--- application.py
+++ application.py
@@ -1,24 +1,25 @@
-from flask import jsonify
-from flask import Flask
-import numpy as np
+# from flask import jsonify
+# from flask import Flask
+# import numpy as np
 
-application = Flask(__name__)
+# application = Flask(__name__)
 
-@application.route('/')
-def hello():
-    return 'Hello Sina\n'
+# @application.route('/')
+# def hello():
+#     return 'Hello Sina\n'
 
-@application.route('/random/<n>')
-def randomvalues(n):
-    values = np.random.randint(0,10,int(n))
-    result = {'values' : values.tolist()}
-    return jsonify(result)
+# @application.route('/random/<n>')
+# def randomvalues(n):
+#     values = np.random.randint(0,10,int(n))
+#     result = {'values' : values.tolist()}
+#     return jsonify(result)
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     
-    application.run(debug=True)
+#     application.run(debug=True)
 
-#     application.run('127.0.0.1', port=5000, debug=True)
+# #     application.run('127.0.0.1', port=5000, debug=True)
     
-#     application.debug = True
-#     application.run(port=5000)
+# #     application.debug = True
+# #     application.run(port=5000)
+print('hello world')
