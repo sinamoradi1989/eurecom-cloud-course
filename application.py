0018e509b9f8bb31fc48f83107c2a620cb1ef228
sinamoradi1989 <94011603+sinamoradi1989@users.noreply.github.com>
2021-11-10 14:57:12 +0100

    Update application.py

--- application.py
+++ application.py
@@ -15,9 +15,10 @@ def randomvalues(n):
     return jsonify(result)
 
 if __name__ == '__main__':
-    application.run('localhost', 5000,debug=True)
+    
+    application.run(debug=True)
 
-#     application.run(debug=True)
+#     application.run('localhost', 5000,debug=True)
     
 #     application.debug = True
 #     application.run(port=5000)
